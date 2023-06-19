7db31ce419d902c7b468c653205520d04714065c
Yupi93 <elboujarfaouiayou@gmail.com>
2023-06-19 11:03:22 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - point enpty
+ * Return: Awlays 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+
+		putchar('\n');
+		return (0);
+}
