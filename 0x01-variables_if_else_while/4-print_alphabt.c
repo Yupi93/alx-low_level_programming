a039839ef195cc896d67d2d17547b232ab7d9f51
Yupi93 <elboujarfaouiayou@gmail.com>
2023-06-19 11:07:59 -0700

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - point enpty
+ * main - Entry point
  * Return: Awlays 0 (Success)
  */
 
@@ -14,7 +14,7 @@ int main(void)
 			if ((c != 'q' && c != 'e') && c <= 'z')
 				putchar(c);
 			c++;
-
+		}
 		putchar('\n');
 		return (0);
 }
